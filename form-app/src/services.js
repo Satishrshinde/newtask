7b630fd76854da01c21c5ff07c83780632b965ff
Satishrshinde <satishrshinde2014@gmail.com>
2023-06-25 17:50:35 +0530

    updated search functionality

--- form-app/src/services.js
+++ form-app/src/services.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import Form from './component/Form';
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import Form from "./component/Form";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA1TuDOIMeHQtSWVbPMHfJBIusFpKyA9Xo",
@@ -11,30 +11,27 @@ const firebaseConfig = {
   storageBucket: "new-form-76bb2.appspot.com",
   messagingSenderId: "638224132908",
   appId: "1:638224132908:web:d3b9c921aa37c7d4ad5a89",
-  measurementId: "G-75ZJCKE56N" 
+  measurementId: "G-75ZJCKE56N",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const Services = () => {
-  const[data,setData]=useState([])
-    const fetchData = async () => {
-      const db = firebase.firestore();
-      const collectionRef = db.collection('tasks');
-      const snapshot = await collectionRef.get();
-      const retrievedData = snapshot.docs.map((doc) => doc.data());
-      setData(retrievedData);
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const db = firebase.firestore();
+    const collectionRef = db.collection("tasks");
+    const snapshot = await collectionRef.get();
+    const retrievedData = snapshot.docs.map((doc) => doc.data());
+    setData(retrievedData);
+  };
 
-    };
-    
   useEffect(() => {
-  fetchData();
-}, [])
+    fetchData();
+  }, []);
   return (
     <div>
-      <Form data={data} 
-      fetchData={fetchData} 
-    />
+      <Form data={data} fetchData={fetchData} />
     </div>
   );
 };
